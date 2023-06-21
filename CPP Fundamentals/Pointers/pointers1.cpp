271c3e5589749fe2d6a7602882387441a607a7c4
Avinash <avinashkumar200217@gmail.com>
2023-06-21 18:20:29 +0530

    Added in Pointers

--- CPP Fundamentals/Pointers/pointers1.cpp
+++ CPP Fundamentals/Pointers/pointers1.cpp
@@ -7,13 +7,25 @@ int main()
 
     int a=3;
     int* b=&a;
+    cout<<a;
     cout<<"Address of a is: "<<b<<endl;
     cout<<"Address of a is: "<<&a<<endl;
-    
+    //derefencing the pointer
+    cout<<*b<<endl;
+
+    float x=10.4;
+    cout<<"Address of x is: "<<&x<<endl;
 
 
     //& ---> Address of operator
 
     // * ---> Dereference operator
     cout<<"The value at address b is: "<<*b<<endl;
+
+    int var=5;
+    int *p;
+    p=&var;
+
+    cout<<var<<endl;
+    cout<<p<<endl; 
 }
