0443f48863251072a9c067b4e2dacd6c1c9379b3
Avinash <avinashkumar200217@gmail.com>
2023-06-22 21:09:53 +0530

    Added Abstraction file

---
+++ CPP Fundamentals/OOP.cpp/Abstraction/Intro.cpp
@@ -0,0 +1,13 @@
+// Abstraction means only displaying essential information and hiding the details.
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int n=2;
+    int power=3;
+    int answer=pow(n,power);
+    cout<<answer;
+}
