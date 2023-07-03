8698ebf7bfafa255b0af068985e49060396145ad
Avinash <avinashkumar200217@gmail.com>
2023-07-03 22:44:40 +0530

    Questions in 2D arrays and some minor changes

---
+++ Practice Questions/2D_array_search.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rows,columns;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+    cout<<"Enter the number of columns: ";
+    cin>>columns;
+    int* array[]
+}
