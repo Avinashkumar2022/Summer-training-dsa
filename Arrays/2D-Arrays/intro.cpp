c910127e797bdfdb95157c3e17cd9b250cf92aa5
Avinash <avinashkumar200217@gmail.com>
2023-07-03 21:43:05 +0530

    Renamed array sorting and some minor edits

--- Arrays/2D-Arrays/intro.cpp
+++ Arrays/2D-Arrays/intro.cpp
@@ -4,14 +4,41 @@ using namespace std;
 int main()
 {
     //Create a 2D array-
-
+    int arr[3][4];
     int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
 
-    for(int i=0;i<3;i++)
+
+/*
+    Row wise traversal of the 2D array
+
+    for (int row = 0; row < 3; row++)
     {
-        for(int j=0;j<4;j++)
+        for (int column = 0; column < 4; column++)
         {
-            cout<<arr[i][j]<<" ";
+            cin>>arr[row][column];
         }
+        
+    }
+*/
+
+/*
+    Column wise traversal in 2D array
+    for (int row = 0; row < 3; row++)
+    {
+        for (int column = 0; column < 4; column++)
+        {
+            cin>>arr[column][row];
+        }
+    }
+    
+*/
+
+    for(int row=0;row<3;row++)
+    {
+        for(int column=0;column<4;column++)
+        {
+            cout<<arr[row][column]<<"\t";
+        }
+        cout<<endl;
     }
 }
