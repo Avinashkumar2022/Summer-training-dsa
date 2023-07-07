48752d14bb9df5884b71e5474411768ed51e6943
Avinash <avinashkumar200217@gmail.com>
2023-07-07 22:57:20 +0530

    Merge sort

--- Arrays sorting.cpp/Merge_Sort.cpp
+++ Arrays sorting.cpp/Merge_Sort.cpp
@@ -1,30 +1,67 @@
 #include<iostream>
 using namespace std;
 
-int mergeSort(int* array,int size)
+void merge(int* arr,int left,int right)
 {
-    
+    int mid=left+((right-left)/2);
+    int lenght1=mid-left+1;
+    int length2=right-mid;
+
+    int* first=new int[lenght1];
+    int* second=new int[length2];
+
+    int pointer=left;
+    for(int i=0;i<lenght1;i++)
+    {
+        first[i]=arr[pointer++];
+    }
+
+    pointer=mid+1;
+    for(int i=0;i<length2;i++)
+    {
+        second[i]=arr[pointer++];
+    }
+
+
+}
+
+void mergeSort(int* array,int left,int right)
+{
+    if(left==right)
+    {
+        return;
+    }
+    int mid=left+((right-left)/2);
+
+    //sort the left side
+    mergeSort(array,left,mid);
+
+    //sort the right side
+    mergeSort(array,mid+1,right);
+
+    //merge the array again
+    merge(array,left,right);
 }
 
 int main()
 {
-    int num_of_elements;
+    int size;
     cout<<"Enter the total number of elements in the array: ";
-    cin>>num_of_elements;
+    cin>>size;
 
-    int* array_Of_Integers=new int[num_of_elements];
+    int* array=new int[size];
 
-    for(int i=0;i<num_of_elements;i++)
+    for(int i=0;i<size;i++)
     {
-        cin>>array_Of_Integers[i];
+        cin>>array[i];
     }
 
-    mergeSort(array_Of_Integers,num_of_elements);
+    mergeSort(array,0,size-1);
     cout<<"Sorted array: ";
-    for(int i=0;i<num_of_elements;i++)
+    for(int i=0;i<size;i++)
     {
-        cout<<array_Of_Integers[i]<<" ";
+        cout<<array[i]<<" ";
     }
 
-    delete[] array_Of_Integers;
+    delete[] array;
 }
