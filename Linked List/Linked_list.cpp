48301d3cc816dfe890e50495a8f84d626a22e30c
Avinash <avinashkumar200217@gmail.com>
2023-11-17 16:45:14 +0530

    minor changes

---
+++ Linked List/Linked_list.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *next;
+    
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+void insertAtTail(Node* &head,int val)
+{
+    Node *n=new Node(val);
+    Node *temp=head;
+    if(head==NULL)
+    {
+        head=n;
+        return;
+    }
+
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+int main()
+{
+    Node* head=NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+
+    Node *temp=head;
+    while(temp->next!=NULL)
+    {
+        cout<<temp->data<<" -> ";
+        temp=temp->next;
+    }
+    cout<<"NULL";
+
+    return 0;
+}
